2487891c33d8feb7da8dab211c38d81e26f5602b
Kriszha Real <yujmin901205@gmail.com>
2024-10-02 05:57:57 +0800

    Add project files.

---
+++ StringAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Yujin";
+            string lName = "Ahn";
+            string greeting = "Welcome, ";
+
+            //concat
+            string sentence = greeting + fName + " " + lName + "!";
+            Console.WriteLine(sentence);
+
+            //convert to uppercase
+            string lowerCase = "this will turn into uppercase.";
+            string upperCase = lowerCase.ToUpper(); 
+            Console.WriteLine(upperCase);
+
+            //stringbuilder
+            StringBuilder paragraph = new StringBuilder();
+
+            //append to string builder
+            paragraph.Append("First sentence here. ");
+            paragraph.Append("Then, second sentence. ");
+            paragraph.Append("Lastly, third sentence.");
+
+            Console.WriteLine(paragraph.ToString());
+            Console.ReadLine();
+        }
+    }
+}
